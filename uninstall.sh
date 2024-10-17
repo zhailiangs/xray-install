376190224f44d0ae97ad5e5169132e5740e517e7
zhailiangs <31151699+zhailiangs@users.noreply.github.com>
2024-10-17 09:42:39 +0800

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-systemctl stop xray
-echo "xray 已停止运行"
-rm -rf /usr/bin/xray
-rm -rf /etc/xray/config.json
-systemctl disable xray
-rm -rf /etc/systemd/system/xray.service
-echo "xray 已卸载"
